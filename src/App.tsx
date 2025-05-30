012160ed39fe71b9636abb1b93195f18dc8da186
“computerFriend2” <allieSouth244@gmail.com>
2025-05-30 10:07:12 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import bulbasaurLogo from '/bulbasaur.svg'
+import LetterRow from './LetterRow'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <img src={bulbasaurLogo} className="logo" alt="bulbasaur logo" />
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Fake Wordle</h1>
+      <div>
+        <p>Input 5-letter word guesses until you guess the secret word!</p>
+        <LetterRow />
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        It's not the real wordle. Don't sue me.
       </p>
     </>
   )
