a0a0a29eae438a4db08fb01b8dd0c152230018ae
“computerFriend2” <allieSouth244@gmail.com>
2025-06-12 09:14:44 -0400

    word grid weird css

--- src/WordGrid.tsx
+++ src/WordGrid.tsx
@@ -1,7 +1,14 @@
 import LetterRow from './LetterRow'
 
+// TODO: find a way to update locked boxes without infinite state updates... might need to manage locked state from this level?
+
 const WordGrid: React.FC = () => {
-    return <div className='letterGrid'>Word Grid Stub
+    return <div className='wordGrid'>
+        <LetterRow />
+        <LetterRow />
+        <LetterRow />
+        <LetterRow />
+        <LetterRow />
         <LetterRow />
     </div>
 }
