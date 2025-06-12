c8140ca75c88e9b2d1139ce43ffbdd07b3be286c
“computerFriend2” <allieSouth244@gmail.com>
2025-06-12 09:16:52 -0400

    row locking after guess submit

--- src/LetterBox.tsx
+++ src/LetterBox.tsx
@@ -3,16 +3,17 @@ import './LetterBox.css'
 
 interface LetterBoxProps {
     letter: string,
-    updateLetter: (arg: string) => void
+    updateLetter: (arg: string) => void,
+    locked: boolean
 }
 
 
 
-const LetterBox: React.FC<LetterBoxProps> = ({ letter, updateLetter }) => {
+const LetterBox: React.FC<LetterBoxProps> = ({ letter, updateLetter, locked }) => {
     const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         updateLetter(event.target.value as string);
     };
-    return <input type="text" className='letterBox'
+    return <input type="text" className='letterBox' disabled={locked}
         maxLength={1} value={letter} onChange={handleChange}></input>
 }
 export default LetterBox;
